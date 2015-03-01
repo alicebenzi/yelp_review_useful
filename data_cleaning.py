2be9a9136219ba349453e03f535e01402f00ff1e
Rachan Bassi <rachanbassi@Rachans-MacBook-Pro.local>
2015-03-01 02:32:04 -0800

    created features freshness and ZIP

--- data_cleaning.py
+++ data_cleaning.py
@@ -15,6 +15,7 @@ data_bs_test = pd.read_table("yelp_test_set_business.csv", sep=",")
 data_ck_test = pd.read_table("yelp_test_set_checkin.csv", sep=",")
 data_rev_test = pd.read_table("yelp_test_set_review.csv", sep=",")
 data_user_test = pd.read_table("yelp_test_set_user.csv", sep=",")
+
 # adding columns row wise for data_checkin
 data_ck_train["total_checkins"] = data_ck_train.sum(axis=1)
 data_ck_test["total_checkins"] = data_ck_train.sum(axis=1)
@@ -23,8 +24,6 @@ data_ck_test["total_checkins"] = data_ck_train.sum(axis=1)
 data_ck_train = data_ck_train.fillna(0)
 data_ck_test = data_ck_test.fillna(0)
 
-
-
 # new check in dataframe with just total check-in's
 data_ck = data_ck_train[["business_id","total_checkins"]]
 data_ck_test_new = data_ck_train[["business_id","total_checkins"]]
@@ -32,7 +31,13 @@ data_ck_test_new = data_ck_train[["business_id","total_checkins"]]
 
 # freshness of a review
 data_rev_train["freshness"] =pd.to_datetime('2013-01-19')- pd.to_datetime(data_rev_train["date"])
+data_rev_train["freshness"] = data_rev_train["freshness"]/ np.timedelta64(1, 'D')
+# print data_rev_train.head()
+
 data_rev_test["freshness"] = pd.to_datetime('2013-03-12')- pd.to_datetime(data_rev_train["date"])
+data_rev_test["freshness"] = data_rev_test["freshness"]/ np.timedelta64(1, 'D')
+# print data_rev_test.head()
+
 
 # features from review text
 adj = []
@@ -83,6 +88,20 @@ for i in range(data_rev_test.shape[0]):
 data_rev_test["count_adj"] = pd.DataFrame(adj)
 data_rev_test["count_punct"] = pd.DataFrame(punct)
 print data_rev_test.head()
+
+# create new feature ZIP
+ZIP = []
+for i in range(data_bs_train.shape[0]):
+    ZIP.append(data_bs_train["full_address"][i][-5:])
+data_bs_train["ZIP"] = pd.DataFrame(ZIP)
+print data_bs_train.head()
+
+ZIP_test = []
+for i in range(data_bs_test.shape[0]):
+    ZIP_test.append(data_bs_test["full_address"][i][-5:])
+data_bs_test["ZIP"] = pd.DataFrame(ZIP_test)
+print data_bs_train.head()
+
 
 #data_merge1 =  pd.merge(data_rev_train,data_bs_train, on ='business_id')
 #data_merge2 = pd.merge(data_merge1,data_ck,on='business_id')
