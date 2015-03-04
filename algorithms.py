61f72fe3ebc0eca309264c5c1174887f58917842
Alice Benziger <alice.benzi@gmail.com>
2015-03-04 15:25:47 -0800

    with count vectorizer encoding

--- algorithms.py
+++ algorithms.py
@@ -2,18 +2,27 @@ __author__ = 'alicebenziger'
 
 from data_encoding import train_test
 from sklearn.svm import SVR
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
 from sklearn.cross_validation import cross_val_score
 
+def rsmle_(predicted,actual):
+   return np.sqrt(np.mean((pow(np.log(predicted+1) - np.log(actual+1),2))))
 
 def support_vector_regressor(X_train, y_train):
     clf = SVR()
     clf.fit(X_train, y_train)
     # plot_learning_curve(clf, title ="Support Vector learning curve", X = X_train,y = y_train, ylim=(0, 1.1))
-    y_pred_SVC = clf.predict(X_train)
+    #y_pred_SVC = clf.predict(X_train)
+    #print clf.predict(X_train)
+    pd.DataFrame(clf.predict(X_train)).to_csv("y_pred_svr.csv")
+    print np.sqrt(np.mean((pow(np.log(clf.predict(X_train)+1) - np.log(y_train+1),2))))
     # plot_validation_curve(clf,title ="Support Vector Classifier validation curve", X=X_train, y=y_train,param_name='C', param_range = [1, 5, 20, 50])
-    print y_train, y_pred_SVC
-    print clf.score(X_train, y_train)
-    # cross_validation_accuracy= cross_val_score(clf, X_train, y_train, cv = 5, scoring = 'accuracy').mean()
+    # print y_train, y_pred_SVC
+    # print clf.score(X_train, y_train)
+    # # cross_validation_accuracy= cross_val_score(clf, X_train, y_train, cv = 5, scoring = 'accuracy').mean()
     # print "Support Vector Classifier : Training set metrics"
     # print "Cross validation accuracy:", cross_validation_accuracy
     # print_metrics(y_train, y_pred_SVC)
@@ -24,5 +33,13 @@ def support_vector_regressor(X_train, y_train):
 if __name__ == '__main__':
 
     train_x, train_y,test_x, test_y, train_x_norm, test_x_norm, pred_x = train_test()
-    support_vector_regressor(train_x_norm,train_y)
+
+
+    rf = RandomForestRegressor()
+    rf.fit(train_x, train_y)
+    print rf.score(train_x,train_y)
+    print rf.score(test_x,test_y)
+    print rsmle_(rf.predict(test_x),test_y)
+    #print train_x[1:5,], train_y[1:5], test_x[1:5,],test_y[1:5]
+    # support_vector_regressor(train_x_norm,train_y)
 
--- data_encoding.py
+++ data_encoding.py
@@ -4,27 +4,48 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 import numpy as np
 from sklearn import preprocessing
 from sklearn.svm import SVR
+from sklearn.feature_extraction.text import CountVectorizer
 
 
-def label_encoder(data, binary_cols, categorical_cols):
+# def label_encoder(data, binary_cols, categorical_cols):
+#     label_enc = LabelEncoder()
+#     for col in categorical_cols:
+#         label_enc.fit(data[col])
+#         data[col] = label_enc.transform(data[col])
+#     encoded_categorical = np.array(data[categorical_cols])
+#
+#     for col in binary_cols:
+#         label_enc.fit(data[col])
+#         data[col] = label_enc.transform(data[col])
+#     encoded_binary = np.array(data[binary_cols])
+#     return encoded_categorical, encoded_binary
+
+def label_encoder(data, binary_cols):
     label_enc = LabelEncoder()
-    for col in categorical_cols:
-        label_enc.fit(data[col])
-        data[col] = label_enc.transform(data[col])
-    encoded_categorical = np.array(data[categorical_cols])
 
     for col in binary_cols:
         label_enc.fit(data[col])
         data[col] = label_enc.transform(data[col])
     encoded_binary = np.array(data[binary_cols])
-    return encoded_categorical, encoded_binary
+    return  encoded_binary
 
 
 def dummy_encoder(train_X,test_X,categorical_variable_list):
-    enc = OneHotEncoder(categorical_features=categorical_variable_list)
+    enc = OneHotEncoder(n_values ='auto',categorical_features=categorical_variable_list)
     train_X = enc.fit_transform(train_X).toarray()
     test_X = enc.transform(test_X).toarray()
     return train_X, test_X
+
+def count_vectoriser(text_col):
+    vect = CountVectorizer(tokenizer=lambda text: text.split(','))
+    cat_fea = vect.fit_transform(text_col.fillna(''))
+    cat_fea = cat_fea.todense()
+    idx_max_1 = cat_fea > 1
+    cat_fea[idx_max_1] = 1
+
+    return cat_fea
+
+
 
 
 def normalize(X):
@@ -44,12 +65,12 @@ def train_test():
     del train["business_id"], train["date"], train["review_id"], train["text"], train["type_x"], train["user_id"],\
         train["votes_cool_x"], train["votes_funny_x"],train["full_address"], train["latitude"], train["longitude"],\
         train["name_x"], train["neighborhoods"], train["type_y"], train["name_y"], train["type"], train["votes_cool_y"],\
-        train["votes_funny_y"], train["votes_useful_y"]
+        train["votes_funny_y"], train["votes_useful_y"], train["state"]
 
     #print test.columns.values
     del pred["business_id"], pred["date"], pred["review_id"], pred["text"], pred["type_x"], pred["user_id"],\
         pred["full_address"], pred["latitude"], pred["longitude"],\
-        pred["name_x"], pred["neighborhoods"], pred["type_y"], pred["name_y"], pred["type"]
+        pred["name_x"], pred["neighborhoods"], pred["type_y"], pred["name_y"], pred["type"], pred["state"]
 
     col_names_train= train.columns.values
     col_names_pred = pred.columns.values
@@ -59,18 +80,40 @@ def train_test():
     target = np.array(train["votes_useful_x"])
     target_var = ["votes_useful_x"]
     binary_var = ["open"]
-    categorical_var = ["categories","city", "state", "zip_code"]
+    categorical_var = ["categories", "city", "zip_code"]
+
+    #categorical_var = ["city"]
+
+    category_encoded = count_vectoriser(train["categories"])
+    zip_encoded = count_vectoriser(train["zip_code"])
+    city_encoded = count_vectoriser(train["city"])
+    #open_encoded = count_vectoriser(train["open"])
+    categorical_variables = np.concatenate((category_encoded,zip_encoded,city_encoded),axis=1)
+
+    category_encoded_pred = count_vectoriser(pred["categories"])
+    zip_encoded_pred = count_vectoriser(pred["zip_code"])
+    city_encoded_pred = count_vectoriser(pred["city"])
+    #open_encoded = count_vectoriser(train["open"])
+    categorical_variables = np.concatenate((category_encoded,zip_encoded,city_encoded),axis=1)
+    categorical_variables_pred = np.concatenate((category_encoded_pred,zip_encoded_pred,city_encoded_pred),axis=1)
+
+    # print category_encoded
+    # print zip_encoded
+    #print categorical_variables
     numerical_var = [col for col in col_names_train if col not in binary_var if col not in categorical_var if col not in target_var]
     numerical_var_pred = [col for col in col_names_pred if col not in binary_var if col not in categorical_var]
-    encoded_categorical, encoded_binary = label_encoder(train, binary_var, categorical_var)
-    encoded_categorical_pred, encoded_binary_pred= label_encoder(pred, binary_var, categorical_var)
+    # encoded_categorical, encoded_binary = label_encoder(train, binary_var, categorical_var)
+    # encoded_categorical_pred, encoded_binary_pred= label_encoder(pred, binary_var, categorical_var)
+    encoded_binary = label_encoder(train, binary_var)
+    encoded_binary_pred= label_encoder(pred, binary_var)
 
     numerical_data = np.array(train[numerical_var])
     numerical_data_pred = np.array(pred[numerical_var_pred])
 
 
-    training_data_transformed = np.concatenate((encoded_categorical,encoded_binary,numerical_data),axis=1)
-    pred_data_transformed = np.concatenate((encoded_categorical_pred,encoded_binary_pred,numerical_data_pred),axis=1)
+    training_data_transformed = np.concatenate((categorical_variables,encoded_binary,numerical_data),axis=1)
+    pred_data_transformed = np.concatenate((categorical_variables_pred,encoded_binary_pred,numerical_data_pred),axis=1)
+
 
 
     #train-test split
@@ -80,12 +123,14 @@ def train_test():
     test_y = target[160379:200473]
     pred_x = pred_data_transformed
 
-
+    pd.DataFrame(test_x).to_csv("test_labelenc.csv")
+    train_x, test_x = dummy_encoder(train_x, test_x, categorical_variable_list = list(range(0,2,1)))
 
     #normalizing (if required)
     train_x_norm = normalize(train_x)
     test_x_norm = normalize(test_x)
 
+    # print train_x, train_y
     return train_x, train_y,test_x, test_y, train_x_norm, test_x_norm, pred_x
 
 
@@ -95,5 +140,4 @@ def train_test():
     # clf = SVR(C=1.0, epsilon=0.2)
     # clf.fit(train_x,train_y)
     # print clf.score(test_x, test_y)
-
-
+train_x, train_y,test_x, test_y, train_x_norm, test_x_norm, pred_x = train_test()
