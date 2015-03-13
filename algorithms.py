209ee744d03b5483313b7f82ec36891fdbda1caa
Alice Benziger <alice.benzi@gmail.com>
2015-03-13 10:01:58 -0700

    Modified/Cleaned py files

--- algorithms.py
+++ algorithms.py
@@ -1,31 +1,22 @@
 __author__ = 'alicebenziger'
 
 from data_encoding import train_test
-# from sklearn.svm import LinearSVR
 import numpy as np
-import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.metrics import make_scorer
-from sklearn.grid_search import GridSearchCV
 import matplotlib.pyplot as plt
 from sklearn.learning_curve import validation_curve
 from sklearn.learning_curve import learning_curve
-
-
-
+from sklearn.linear_model import LinearRegression,Ridge
 
 
 def rsmle_(predicted,actual):
-    print " i am called..."
     actual = np.exp(actual)-1
     predicted = np.exp(predicted)-1
     return np.sqrt(np.mean((pow(np.log(predicted+1) - np.log(actual+1),2))))
 
-#customising the score for cross validation
-rsmle_score = make_scorer(rsmle_,greater_is_better=False)
 
 def plot_validation_curve(estimator, title, X, y, param_name, param_range, ylim=None):
     plt.figure()
@@ -54,6 +45,7 @@ def plot_validation_curve(estimator, title, X, y, param_name, param_range, ylim=
     plt.legend(loc="best")
     plt.show()
 
+
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
                         n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
 
@@ -63,18 +55,15 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
         plt.ylim(*ylim)
     plt.xlabel("Training examples")
     plt.ylabel("RMSLE")
-
     rsmle_score = make_scorer(rsmle_,greater_is_better=True)
-
     train_sizes, train_scores, test_scores = learning_curve(
-    estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes,scoring = rsmle_score)
+    estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, scoring = rsmle_score)
     print "plotting learning curve.."
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
     test_scores_std = np.std(test_scores, axis=1)
     plt.grid()
-
     plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
                      train_scores_mean + train_scores_std, alpha=0.1,
                      color="r")
@@ -84,37 +73,32 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
              label="Training score")
     plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
              label="Cross-validation score")
-
     plt.legend(loc="best")
     plt.show()
 
 
-def random_forest_regressor(train_x, train_y, pred_x, review_id, v_curve=False, l_curve=False, get_model=False):
-    # rf = RandomForestRegressor()
+def random_forest_regressor(train_x, train_y, pred_x, review_id, v_curve=False, l_curve=False, get_model=True):
     rf = RandomForestRegressor(n_estimators=20,criterion='mse',max_features='auto', max_depth=10)
     if get_model:
-
+        print "Fitting RF..."
         rf.fit(train_x, np.log(train_y+1))
         print rf.score(train_x, np.log(train_y+1))
         rf_pred = np.exp(rf.predict(pred_x))-1.0
-        # print rf.feature_importances_
-
         Votes = rf_pred[:,np.newaxis]
         Id = np.array(review_id)[:,np.newaxis]
-        print len(Votes), len(Id)
-        submission_rf= np.concatenate((Id,Votes),axis=1)
-        # print submission_rf
-
+        submission_rf = np.concatenate((Id,Votes),axis=1)
+        # create submission csv for Kaggle
         np.savetxt("submission_rf.csv", submission_rf,header="Id,Votes", delimiter=',',fmt="%s, %0.2f", comments='')
+    # plot validation and learning curves
     if v_curve:
         train_y = np.log(train_y+1.0)
-        plot_validation_curve(rf, "Random Forest: Validation Curve(No: of tree)", train_x,train_y,'n_estimators',[5,10,20,50,100])
+        plot_validation_curve(RandomForestRegressor(), "Random Forest: Validation Curve(No: of trees)", train_x,train_y,'n_estimators',[5,10,20,50,100])
     if l_curve:
         train_y = np.log(train_y+1.0)
-        plot_learning_curve(rf,"Rf: Learning Curve", train_x,train_y)
+        plot_learning_curve(RandomForestRegressor(), "Random Forest: Learning Curve", train_x,train_y)
 
 
-def ada_boost_regressor(train_x, train_y, pred_x, review_id, v_curve=False, l_curve=False, get_model=False):
+def ada_boost_regressor(train_x, train_y, pred_x, review_id, v_curve=False, l_curve=False, get_model=True):
         ada = AdaBoostRegressor(n_estimators=5)
         if get_model:
             print "Fitting Ada..."
@@ -125,52 +109,91 @@ def ada_boost_regressor(train_x, train_y, pred_x, review_id, v_curve=False, l_cu
             # create submission csv for Kaggle
             submission_ada= np.concatenate((Id,Votes),axis=1)
             np.savetxt("submission_ada.csv", submission_ada,header="Id,Votes", delimiter=',',fmt="%s, %0.2f", comments='')
-
         # plot validation and learning curves
         if l_curve:
             print "Working on Learning Curves"
-            plot_learning_curve(AdaBoostRegressor(), "Learning curve for Adaboost", train_x, np.log(train_y+1.0))
+            plot_learning_curve(AdaBoostRegressor(), "Learning curve: Adaboost", train_x, np.log(train_y+1.0))
         if v_curve:
             print "Working on Validation Curves"
-            plot_validation_curve(AdaBoostRegressor(), "Validation Curve for Adaboost", train_x, np.log(train_y+1.0),
-                              param_name="n_estimators", param_range=[2,5,10,15,20,25,30])
+            plot_validation_curve(AdaBoostRegressor(), "Validation Curve: Adaboost", train_x, np.log(train_y+1.0),
+                                  param_name="n_estimators", param_range=[2, 5, 10, 15, 20, 25, 30])
 
-def gradient_boosting_regressor(train_x, train_y, pred_x, review_id, v_curve = False, l_curve = False, get_model = False):
-        gbr = GradientBoostingRegressor(n_estimators=400, max_depth=7, random_state=7)
+
+def gradient_boosting_regressor(train_x, train_y, pred_x, review_id, v_curve=False, l_curve=False, get_model=True):
+        gbr = GradientBoostingRegressor(n_estimators=200, max_depth=7, random_state=7)
         if get_model:
             print "Fitting GBR..."
             gbr.fit(train_x, np.log(train_y+1))
             gbr_pred = np.exp(gbr.predict(pred_x))- 1
-
+            #dealing with
             for i in range(len(gbr_pred)):
                 if gbr_pred[i] < 0:
                     gbr_pred[i] = 0
-
-            Votes = gbr_pred[:,np.newaxis]
-            Id = np.array(review_id)[:,np.newaxis]
-
-            submission_gbr= np.concatenate((Id,Votes),axis=1)
+            Votes = gbr_pred[:, np.newaxis]
+            Id = np.array(review_id)[:, np.newaxis]
+            submission_gbr = np.concatenate((Id,Votes),axis=1)
             np.savetxt("submission_gbr.csv", submission_gbr,header="Id,Votes", delimiter=',',fmt="%s, %0.2f", comments='')
-
+        # plot validation and learning curves
         if v_curve:
             print "Working on Validation Curves"
-            plot_validation_curve(GradientBoostingRegressor(), "Validation Curve for GBR", train_x, np.log(train_y+1.0),
-                              param_name="n_estimators", param_range=[5,20, 60, 100, 150, 200])
+            plot_validation_curve(GradientBoostingRegressor(), "Validation Curve: GBR", train_x, np.log(train_y+1.0),
+                                  param_name="n_estimators", param_range=[5, 20, 60, 100, 150, 200])
         if l_curve:
             print "Working on Learning Curves"
-            plot_learning_curve(GradientBoostingRegressor(), "Learning Curve for GBR", train_x, np.log(train_y+1.0))
+            plot_learning_curve(GradientBoostingRegressor(), "Learning Curve: GBR", train_x, np.log(train_y+1.0))
 
+
+def linear_regression(train_x, train_y, pred_x, review_id, v_curve=False, l_curve=False, get_model=True):
+    lin = LinearRegression(normalize=True)
+    if get_model:
+        print "Fitting Linear..."
+        lin.fit(train_x, np.log(train_y+1))
+        gbr_pred = np.exp(lin.predict(pred_x))- 1
+        for i in range(len(gbr_pred)):
+            if gbr_pred[i] < 0:
+                gbr_pred[i] = 0
+        Votes = gbr_pred[:,np.newaxis]
+        Id = np.array(review_id)[:,np.newaxis]
+        submission_lin= np.concatenate((Id,Votes),axis=1)
+        np.savetxt("submission_lin.csv", submission_lin,header="Id,Votes", delimiter=',',fmt="%s, %0.2f", comments='')
+    # plot validation and learning curves
+    if v_curve:
+        pass
+    if l_curve:
+        print "Working on Learning Curves"
+        plot_learning_curve(LinearRegression(), "Learning Curve for Linear Regression", train_x, np.log(train_y+1.0))
+
+
+def ridge_regression(train_x, train_y, pred_x, review_id, v_curve=False, l_curve=False, get_model=True):
+    lin = Ridge(alpha=0.5)
+    if get_model:
+        print "Fitting Ridge..."
+        lin.fit(train_x, np.log(train_y+1))
+        gbr_pred = np.exp(lin.predict(pred_x))- 1
+        for i in range(len(gbr_pred)):
+            if gbr_pred[i] < 0:
+                gbr_pred[i] = 0
+        Votes = gbr_pred[:, np.newaxis]
+        Id = np.array(review_id)[:, np.newaxis]
+        submission_lin= np.concatenate((Id,Votes),axis=1)
+        np.savetxt("submission_ridge.csv", submission_lin,header="Id,Votes", delimiter=',',fmt="%s, %0.2f", comments='')
+    if v_curve:
+        print "Working on Validation Curves"
+        plot_validation_curve(Ridge(), "Validation Curve for Ridge Regression", train_x, np.log(train_y+1.0),
+                              param_name="alpha", param_range=[0.1,0.2,0.5,1,10])
+    if l_curve:
+        print "Working on Learning Curves"
+        plot_learning_curve(Ridge(), "Learning Curve for Linear Regression", train_x, np.log(train_y+1.0))
 
 if __name__ == '__main__':
-
     train_x, train_y, pred_x, train_x_norm, pred_x_norm, review_id = train_test()
-
     print "data fetched..."
-    #random forest regressor
-    # random_forest_regressor(train_x, train_y, pred_x, review_id, get_model=True)
-    # gradient_boosting_regressor(train_x, train_y, pred_x, review_id, get_model = True)
+    random_forest_regressor(train_x, train_y, pred_x, review_id, get_model=True)
+    gradient_boosting_regressor(train_x, train_y, pred_x, review_id, get_model = True)
     ada_boost_regressor(train_x, train_y, pred_x, review_id, get_model=True)
-    print "done.."
+    linear_regression(train_x, train_y, pred_x, review_id, get_model=True)
+    ridge_regression(train_x, train_y, pred_x, review_id, v_curve=True)
+    print "modelling done.."
 
 
 
--- data_encoding.py
+++ data_encoding.py
@@ -8,33 +8,20 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cluster import MiniBatchKMeans
 
 
-
-
 def label_encoder(data, binary_cols):
     label_enc = LabelEncoder()
-    # for col in categorical_cols:
-    #     label_enc.fit(data[col])
-    #     data[col] = label_enc.transform(data[col])
-    # encoded_categorical = np.array(data[categorical_cols])
-
     for col in binary_cols:
         label_enc.fit(data[col])
         data[col] = label_enc.transform(data[col])
     encoded_binary = np.array(data[binary_cols])
-
     return encoded_binary
 
 
-def dummy_encoder(train_X,test_X,categorical_variable_list):
+def dummy_encoder(train_X, test_X, categorical_variable_list):
     enc = OneHotEncoder(n_values ='auto',categorical_features=categorical_variable_list)
-    # train_X = enc.fit_transform(train_X)
-    # test_X = enc.transform(test_X)
-
-    #
     train_X = enc.fit_transform(train_X).toarray()
     test_X = enc.transform(test_X).toarray()
     return train_X, test_X
-
 
 
 def normalize(X):
@@ -55,100 +42,47 @@ def category_manipulation(train,test):
     cat_fea_test = cat_fea_test.todense()
     idx_max_1 = cat_fea_test > 1
     cat_fea_test[idx_max_1] = 1
-
-    # CATEGORY CLUSTERS
-    #  Based on the category extracted before, the idea is to create a n clusters to
-    #  aggregate set of similar categories
-    # for esti in (2,3):#,60,70,80,90,100,110,125):
     esti = 50
-    km = MiniBatchKMeans(n_clusters=esti, random_state=888)#, init_size=esti*10)
-    #       init='k-means++', n_clusters=3, n_init=10
+    km = MiniBatchKMeans(n_clusters=esti, random_state=888)
     km.fit(cat_fea)
-
-    # train['cat_clust_'+str(esti)] = km.predict(cat_fea)
-    # test['cat_clust_'+str(esti)] = km.predict(cat_fea_test)
-    train['cat_clust_100'] = km.predict(cat_fea)
-    test['cat_clust_100'] = km.predict(cat_fea_test)
-    # return train,test
+    train['cat_clust_50'] = km.predict(cat_fea)
+    test['cat_clust_50'] = km.predict(cat_fea_test)
 
 
 def train_test():
-
     train = pd.read_csv('train_new.csv', header=0)
     pred = pd.read_csv('test_new.csv', header=0)
-
-    print train.head(), pred.head()
-
-
-
     category_manipulation(train, pred)
-
     review_id = pred["review_id"]
-
-    #creating new variables
-
-    # train["stars_per_checkin"] = train["stars_bs"]/(train["total_checkins"]+1)
-    # pred["stars_per_checkin"] = pred["stars_bs"]/(pred["total_checkins"]+1)
-
 
     del train["categories"], pred["categories"], train["review_id"], pred["review_id"],train["count_punct"], \
         pred["count_punct"],train["count_adj"], pred["count_adj"], \
         train["freshness"], pred["freshness"], train["stars_bs"], pred["stars_bs"],\
-        train["total_checkins"], pred["total_checkins"]
-        # train["cat_clust_100"], pred["cat_clust_100"], # train["zip_code"], \
-        # pred["zip_code"]#, train["open"], pred["open"], train["total_checkins"], pred["total_checkins"], \
-        # train["count_punct"], pred["count_punct"], train["count_adj"], pred["count_adj"]
-
-
-
-
+        train["total_checkins"], pred["total_checkins"], train["zip_code"], pred["zip_code"]
 
     target = np.array(train["votes_useful_rev"])
     target_var = ["votes_useful_rev"]
     binary_var = ["open"]
-    categorical_var = ["cat_clust_100", "zip_code"]
-    # categorical_var = ["zip_code"]
-
-    col_names_train= train.columns.values
+    categorical_var = ["cat_clust_50","loc_clust_125"]
+    col_names_train = train.columns.values
     col_names_pred = pred.columns.values
-    # print col_names_train
     numerical_var = [col for col in col_names_train if col not in binary_var if col not in categorical_var if col not in target_var]
     numerical_var_pred = [col for col in col_names_pred if col not in binary_var if col not in categorical_var]
-    # numerical_var = [col for col in col_names_train  if col not in categorical_var if col not in target_var]
-    # numerical_var_pred = [col for col in col_names_pred  if col not in categorical_var]
     encoded_binary = label_encoder(train, binary_var)
     encoded_binary_pred= label_encoder(pred, binary_var)
     categorical_variables = np.array(train[categorical_var])
     categorical_variables_pred = np.array(pred[categorical_var])
     numerical_data = np.array(train[numerical_var])
     numerical_data_pred = np.array(pred[numerical_var_pred])
-    #
-    #
-    #
-    training_data_transformed = np.concatenate((categorical_variables,encoded_binary,numerical_data),axis=1)
-    pred_data_transformed = np.concatenate((categorical_variables_pred,encoded_binary_pred,numerical_data_pred),axis=1)
-    # training_data_transformed = np.concatenate((categorical_variables,numerical_data),axis=1)
-    # pred_data_transformed = np.concatenate((categorical_variables_pred,numerical_data_pred),axis=1)
-    #
-    #
-    #
+    training_data_transformed = np.concatenate((categorical_variables, encoded_binary,numerical_data),axis=1)
+    pred_data_transformed = np.concatenate((categorical_variables_pred, encoded_binary_pred,numerical_data_pred),axis=1)
     train_x = training_data_transformed
     train_y = target
     pred_x = pred_data_transformed
-    #
-    # print len(train_x[0]), len(pred_x[0])
-    #
-    pd.DataFrame(train_x).to_csv("test_labelenc.csv")
-
-    train_x, pred_x = dummy_encoder(train_x, pred_x, categorical_variable_list=list(range(0,1)))
-    #
-    #normalizing (if required)
+    train_x, pred_x = dummy_encoder(train_x, pred_x, categorical_variable_list=list(range(0, 2)))
+    #normalizing
     train_x_norm = normalize(train_x)
     pred_x_norm = normalize(pred_x)
-    # print train_x[0:1,:]
-    #
-    #
-    print "done.."
     return train_x, train_y, pred_x, train_x_norm, pred_x_norm, review_id
 
 
