40627741be3b0822bd18be76a484542938adcdb0
Alice Benziger <alice.benzi@gmail.com>
2015-03-13 10:12:14 -0700

    json to csv conversion - code clean up

--- json_csv_conversion.py
+++ json_csv_conversion.py
@@ -4,18 +4,19 @@ import json
 import pandas as pd
 from glob import glob
 
-def convert(x):
+
+def convert(json_file):
     ''' Convert a json string to a flat python dictionary
     which can be passed into Pandas. '''
-    ob = json.loads(x)
-    for k, v in ob.items():
+    load_json = json.loads(json_file)
+    for k, v in load_json.items():
         if isinstance(v, list):
-            ob[k] = ','.join(v)
+            load_json[k] = ','.join(v)
         elif isinstance(v, dict):
             for kk, vv in v.items():
-                ob['%s_%s' % (k, kk)] = vv
-            del ob[k]
-    return ob
+                load_json['%s_%s' % (k, kk)] = vv
+            del load_json[k]
+    return load_json
 
 for json_filename in glob('*.json'):
     csv_filename = '%s.csv' % json_filename[:-5]
